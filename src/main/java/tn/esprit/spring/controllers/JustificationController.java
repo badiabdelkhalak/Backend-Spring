25e98925e2f6f5cea01dd0aacc52098e0cb5f3a2
MeriamEsprit <meriem.hjiri@esprit.tn>
2024-06-01 16:16:44 +0100

    new edits

--- src/main/java/tn/esprit/spring/controllers/JustificationController.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("justification")
-public class JustificationController {
-}
--- src/main/java/tn/esprit/spring/repositories/AttendanceRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.spring.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
-}
