e14f8cc1c2f935dbacb271e47d202bcba7b8afd7
Achemsi <Amine.chemsi@esprit.tn>
2024-06-01 16:03:00 +0100

    delete entity

--- src/main/java/tn/esprit/spring/controllers/AttendanceController.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("attendance")
-public class AttendanceController {
-}
